1d2f3aad49bbbfc1a4c6ca27a2b873a66201c45d
Prince Arora <prnc.arora0@gmail.com>
2019-07-21 17:46:19 +0530

    print function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod print_helper;
+
 fn main() {
     println!("Hello, world!");
+    print_helper::print_name();
 }
---
+++ src/print_helper.rs
@@ -0,0 +1,4 @@
+//Print helper
+pub fn print_name() {
+    println!("My name is {} {}", "Prince", "Arora");
+}
