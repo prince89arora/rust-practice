83b86ff400f0198340c18635f545036932974303
Prince Arora <prnc.arora0@gmail.com>
2019-07-22 22:52:10 +0530

    find max

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod print_helper;
+mod types;
 
 fn main() {
     println!("Hello, world!");
     print_helper::run();
+    types::run();
 }
--- src/types.rs
+++ src/types.rs
@@ -12,10 +12,18 @@ pub fn run() {
 
     // default is i32
     let default_int = 1;
+    println!(" {} ", default_int);
 
     // default is f64
     let default_float = 1.2;
+    println!(" {} ", default_float);
 
     // explicitly given type
     let explicit_type_integer: i64 = 12312312;
+    println!(" {} ", explicit_type_integer);
+
+    //find max for any numeric data type.
+    println!("Max i16: {}", std::i16::MAX);
+    println!("Max i32: {}", std::i32::MAX);
+    println!("Max f32: {}", std::f32::MAX);
 }
