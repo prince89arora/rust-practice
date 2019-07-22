fa71d420d2c7a8a4ab529d6287a7bd0941a7698e
Prince Arora <prnc.arora0@gmail.com>
2019-07-22 22:44:28 +0530

    types declaration

--- src/types.rs
+++ src/types.rs
@@ -10,4 +10,12 @@
 //But its not mandatory to assign type for every single var.
 pub fn run() {
 
+    // default is i32
+    let default_int = 1;
+
+    // default is f64
+    let default_float = 1.2;
+
+    // explicitly given type
+    let explicit_type_integer: i64 = 12312312;
 }
