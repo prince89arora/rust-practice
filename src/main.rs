e7a098e5eb3bbb5565e80e81d6034aab0df60d43
Prince Arora <prnc.arora0@gmail.com>
2019-07-23 21:47:55 +0530

    strings added

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod print_helper;
 mod types;
+mod strings;
 
 fn main() {
     println!("Hello, world!");
     print_helper::run();
     types::run();
+    strings::run();
 }
---
+++ src/strings.rs
@@ -0,0 +1,6 @@
+//primitive strings are fixed length strings in memory.
+//String = growable, Head allocated data structure, used when need to modify string data.
+
+pub fn run() {
+
+}
