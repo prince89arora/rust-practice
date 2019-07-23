ff8f802ff32c1f97ea149782661ca24fc4bdc309
Prince Arora <prnc.arora0@gmail.com>
2019-07-23 21:54:52 +0530

    string edit

--- src/strings.rs
+++ src/strings.rs
@@ -2,8 +2,14 @@
 //String = growable, Head allocated data structure, used when need to modify string data.
 
 pub fn run() {
-
-    let hello = String::from("Hello!!");
+    // Growable String..
+    let mut hello = String::from("Hello!!");
     println!("{}", hello);
     println!("Length: {}", hello.len());
+
+    //push char
+    hello.push(' ');
+    //push string
+    hello.push_str("There...");
+    println!("{}", hello);
 }
