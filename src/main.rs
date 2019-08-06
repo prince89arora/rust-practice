4e2cd4e155f0e6dff9d553fdf239b80fb0564c8e
Prince Arora <prnc.arora0@gmail.com>
2019-08-06 21:49:13 +0530

    touples

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod print_helper;
 mod types;
 mod strings;
+mod touples;
 
 fn main() {
     println!("Hello, world!");
     print_helper::run();
     types::run();
     strings::run();
+
+    touples::run();
 }
--- src/touples.rs
+++ src/touples.rs
@@ -1,3 +1,5 @@
 pub fn run() {
+    let person: (&str, &str, i8) = ("Prince", "Arora", 29);
 
+    println!("{} {} is {} years old.", person.0, person.1, person.2);
 }
