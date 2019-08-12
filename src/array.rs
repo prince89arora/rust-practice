5573119e6d284daf26828d588a52dcf4c61d268f
Prince Arora <prnc.arora0@gmail.com>
2019-08-12 22:41:43 +0530

    memory size for an array

--- src/array.rs
+++ src/array.rs
@@ -1,6 +1,9 @@
 pub fn run() {
-    let arr: [i32; 4] = [1,2,3,4];
+    let arr: [i8; 4] = [1,2,3,4];
 
     // all element print.
     println!("{:?}", arr);
+
+    //get memory size for an array...
+    println!("Consumes {} bytes", std::mem::align_of_val(&arr));
 }
