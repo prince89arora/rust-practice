fffb9d47bd32e2168f2035d779655a5cb9b253f4
Prince Arora <prnc.arora0@gmail.com>
2019-08-13 23:05:08 +0530

    vector insertion

--- src/vectors.rs
+++ src/vectors.rs
@@ -1,6 +1,12 @@
 pub fn run() {
-    let mut vec_example: Vec<i32> = vec![];
+    let mut vec_example: Vec<i32> = vec![1,2,3,4,5,5];
 
     vec_example.insert(0, 2);
     println!("{:?}", vec_example);
+    println!("size {}", vec_example.len());
+    vec_example.insert( vec_example.len(), 5 );
+
+    println!("After Insertion....");
+    println!("{:?}", vec_example);
+    println!("size {}", vec_example.len());
 }
