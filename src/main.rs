953e468bd9e1084a196551e4024717eccc0b5d2a
Prince Arora <prnc.arora0@gmail.com>
2019-09-16 15:43:56 +0530

    guessing game start

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-mod print_helper;
-mod types;
-mod strings;
-mod touples;
-mod array;
-mod vectors;
+//mod print_helper;
+//mod types;
+//mod strings;
+//mod touples;
+//mod array;
+//mod vectors;
 
 fn main() {
-    println!("Hello, world!");
-    print_helper::run();
-    types::run();
-    strings::run();
-
-    touples::run();
-    array::run();
-    vectors::run();
+//    println!("Hello, world!");
+//    print_helper::run();
+//    types::run();
+//    strings::run();
+//
+//    touples::run();
+//    array::run();
+//    vectors::run();
 }
