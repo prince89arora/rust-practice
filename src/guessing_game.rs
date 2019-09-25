2590f8e6ffee4251701db39e8212cefa9557a91b
Prince Arora <prnc.arora0@gmail.com>
2019-09-25 14:40:55 +0530

    match and random number for guess

--- src/guessing_game.rs
+++ src/guessing_game.rs
@@ -1,6 +1,11 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 pub fn run() {
+    let random_num = rand::thread_rng()
+        .gen_range(1, 10);
+
     println!("Guess the number.....!");
     println!("Please enter your number:");
 
@@ -9,5 +14,12 @@ pub fn run() {
         .read_line(&mut guess)
         .expect("Unable to read guessed number..");
 
-    println!("You have guessed: {}", guess);
+    let guess_int: u32 = guess.trim().parse()
+        .expect("Unable to parse guessed input...");
+
+    match guess_int.cmp(&random_num) {
+        Ordering::Less => { println!("low guess...") },
+        Ordering::Equal => { println!("correct guess...") },
+        Ordering::Greater => { println!("High guess...") }
+    }
 }
