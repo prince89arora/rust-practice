53d84e3792006f463dc26d4b674c22cec18964cb
Prince Arora <prnc.arora0@gmail.com>
2019-09-25 15:12:56 +0530

    loop logic

--- src/guessing_game.rs
+++ src/guessing_game.rs
@@ -7,19 +7,42 @@ pub fn run() {
         .gen_range(1, 10);
 
     println!("Guess the number.....!");
-    println!("Please enter your number:");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Unable to read guessed number..");
+    loop {
+        println!("Please enter your number:");
 
-    let guess_int: u32 = guess.trim().parse()
-        .expect("Unable to parse guessed input...");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Unable to read guessed number..");
 
-    match guess_int.cmp(&random_num) {
-        Ordering::Less => { println!("low guess...") },
-        Ordering::Equal => { println!("correct guess...") },
-        Ordering::Greater => { println!("High guess...") }
+        let guess: u32 = match guess.trim().parse() {
+            Ok( num ) => num,
+            Err( _e ) => continue,
+        };
+
+        if validate_input(random_num, guess) {
+            break;
+        }
     }
+}
+
+fn validate_input(random_num:u32, input:u32) -> bool {
+    let flag: bool;
+    match input.cmp(&random_num) {
+        Ordering::Less => {
+            println!("low guess...");
+            flag = false;
+        },
+        Ordering::Equal => {
+            println!("correct guess...");
+            println!("--- You Win!! ---");
+            flag = true;
+        },
+        Ordering::Greater => {
+            println!("High guess...");
+            flag = false;
+        }
+    }
+    return flag;
 }
