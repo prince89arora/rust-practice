a89572dabba830132188c490f8c8cc929ca4f3a3
Prince Arora <prnc.arora0@gmail.com>
2019-10-11 11:59:13 +0530

    for loop with range

--- src/control_flow.rs
+++ src/control_flow.rs
@@ -45,4 +45,21 @@ pub fn run() {
         index_while += 1;
     }
 
+
+    //4. for loop
+    let list_for_one = [1,2,3,4,5,6];
+    for ele in list_for_one.iter() {
+        println!("Item _ For: {}", ele);
+    }
+    
+    //4. for with range
+    for ele in 0..4 {
+        println!("For __ range: {}", list_for_one[ele]);
+    }
+
+    //5. reverse range
+    for index in (0..4).rev() {
+        println!("For __ range reverse: {}", list_for_one[index]);
+    }
+
 }
