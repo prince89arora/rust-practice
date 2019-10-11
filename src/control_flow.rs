b6601eef295f76c215a06ea57c9b342e672833cd
Prince Arora <prnc.arora0@gmail.com>
2019-10-11 10:08:04 +0530

    loops and while

--- src/control_flow.rs
+++ src/control_flow.rs
@@ -15,4 +15,34 @@ pub fn run() {
     };
     println!("Result from assignment {}", result);
 
+
+    //loops
+    //1. Simple loop
+    let mut counter: i32 = 0;
+    loop {
+        counter+=1;
+        println!("Counter is: {} ", counter);
+        if counter == 5 {
+            break;
+        }
+    }
+
+    //2. return value from loop
+    counter = 0;
+    let result_loop_return = loop {
+        counter+=1;
+        if counter == 4 {
+            break counter + 1;
+        }
+    };
+    println!("Counter from assignment {}", result_loop_return);
+
+    //3. while loop.
+    let while_list_ex = [1,2,3,4,5,6];
+    let mut index_while = 0;
+    while index_while < 6 {
+        println!("While loop item: {}", while_list_ex[index_while]);
+        index_while += 1;
+    }
+
 }
