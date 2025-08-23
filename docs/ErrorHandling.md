baff21e87d06fb4c387c90023b752ce226da43e2
Prince Arora <aroraprince151@gmail.com>
2025-08-23 19:27:33 +0100

    error handling in rust

---
+++ docs/ErrorHandling.md
@@ -0,0 +1,14 @@
+# Error Handling
+
+- `$env:RUST_BACKTRACE=1` can be set to enable stacktrace in case of any unrecoverable error.
+
+## Types
+
+1. **Unrecoverable Error** - things that cannot be recovered.
+2. **Recoverable Error**
+
+## Ways to Handle Errors
+
+1. **Handle Error**
+
+2. **Propagate the Error**
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use log::log;
+// use std::fs::File;
+// use std::io::{Error, Read};
+// use std::io::ErrorKind;
 
 #[warn(unused_variables)]
 fn main() {
@@ -41,6 +43,46 @@ fn main() {
     // ================= Closure =====================//
 
 
+    // ================= Error Handling =====================//
+    // let list = vec![1,2,3,4,5,6];
+    // println!("{}", list[8]);
+
+    // // Handle error without propagation
+    // let file_name = "./test.json";
+    // match File::open(file_name) {
+    //     Ok(file) => {
+    //         println!("Reading file {:#?}", file);
+    //     }
+    //     Err(error) => {
+    //         match error.kind() {
+    //             ErrorKind::NotFound => {
+    //                 match File::create("./test.json") {
+    //                     Ok(file) => { println!("Create new file {:#?}", file); }
+    //                     Err(error) => {println!("Error: {:#?}", error); }
+    //                 }
+    //             }
+    //             _ => {
+    //                 println!("Error: {:#?}", error);
+    //             }
+    //
+    //         }
+    //     }
+    // }
+
+
+    // // Error propagation
+    // let file_name = "./test.json";
+    // match read_file(file_name) {
+    //     Ok(contents) => {
+    //         println!("{}", contents);
+    //     }
+    //     Err(error) => {
+    //         println!("{:#?}", error);
+    //     }
+    // }
+    // ================= Error Handling =====================//
+
+
 }
 
 // /// Function to find gigger string from 2 strings and to show how lifetime works
@@ -52,4 +94,13 @@ fn main() {
 //     //! &'a mut str = to define lifetime for a mutable reference type.
 //
 //     if a.len() > b.len() { a } else { b }
+// }
+
+// /// Function to read a file content as string. Propagate if there is any error
+// /// * file_name = file path as string slice.
+// fn read_file(file_name: &str) -> Result<String, Error> {
+//     let mut file = File::open(file_name)?;
+//     let mut file_data_string = String::new();
+//     file.read_to_string(&mut file_data_string)?;
+//     Ok(file_data_string)
 // }
